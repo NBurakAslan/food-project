48ea1d9f634df7b394041e0a11704b0c32de7af0
darkplanetar <burakdarkp@yahoo.com>
2022-04-12 16:42:27 +0300

    12.04.2022

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Layout/Header.js";
+import Meals from "./components/Meals/Meals.js";
+import Cart from "./components/Cart/Cart.js";
+
+function App() {
+  return (
+    <>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
+}
+
+export default App;
